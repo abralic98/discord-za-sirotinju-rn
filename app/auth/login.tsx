ab64ddafd6b5cee92a8042942ef5c82f6cd712c0
abralic98 <twogamers1950@gmail.com>
2025-06-30 14:03:18 +0200

    ínit

---
+++ app/auth/login.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Login() {
+  const kita = false
+  return (
+    <View>
+      <Text className="bg-blue-500">
+        Edit kita app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
